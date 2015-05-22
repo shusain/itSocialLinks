0d37a12dfd9ec32ffb175bed17f3105e2973d1a5
Shaun Husain <shaun@intellectual-tech.com>
2015-05-22 14:48:13 -0500

    Updating the bower.json project name to make sense

--- dist/scripts/mainApp.js
+++
@@ -1,9 +0,0 @@
-
-  /**
-  * mainApp Module
-  *
-  * Just an app for testing out the module
-  */
-  angular.module('mainApp', ["socialMedia"]).controller("TestCtrl", function($scope){
-    $scope.pages = [1,2,3]
-  });
